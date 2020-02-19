8360de00ccabc424541cb6d894593fc226024402
FelipeFranco97 <francozuluaga97@gmail.com>
2020-02-18 23:29:43 -0500

    más botones, cronometro
    Se actualizaron los botones que faltaban por tener el modelo creado por la diseñadora.
    
    En los créditos ahora aparece en emoji haciendo referencia a ella.
    
    Se creo un cronómetro en js para empezar a contabilizar el tiempo mientras la gente juega.

--- js/main.js
+++ js/main.js
@@ -26,3 +26,29 @@ function cambiarSeccion(id_seccion){
     secciones[id_seccion].classList.remove("oculto");
 
 }
+
+function carga(){
+    contador_s=0;
+    contador_m=0;
+    s = document.getElementById("segundos");
+    m = document.getElementById("minutos");
+    
+    window.setInterval(
+        function(){
+            if(contador_s==60){
+                
+                contador_s=0;
+                contador_m++;
+                m.innerHTML = contador_m;
+                
+                if(contador_m==0){
+                    
+                    contador_m=0;
+                }
+            }
+            
+            s.innerHTML = contador_s;
+            contador_s++;
+        }
+        ,1000);
+}
