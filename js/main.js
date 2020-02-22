1fe8ac5af4baf3eb790178efd0a42642e56dad64
FelipeFranco97 <francozuluaga97@gmail.com>
2020-02-21 21:42:04 -0500

    font, img, fix js
    Se agrego la fuente Bebas Neue.
    
    Se colocaron las imagenes del primer nivel.
    
    Se arreglo un problema que tenía el cronómetro el cual nunca se detenía.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
 var secciones = [];
-var imagenes = [];
-var preguntas [];
-
 var tiempo_splash = 3000;
-    
+var id_intervalo;
+
 window.onload = function(){
     inicializarReferencias();
     setTimeout(cambiarSplash,tiempo_splash);
@@ -15,15 +13,8 @@ function inicializarReferencias(){
     secciones[3] = document.getElementById("seccion_3");
     secciones[4] = document.getElementById("seccion_4");
     secciones[5] = document.getElementById("seccion_5");
-    imagenes[1] = document.getElementById("primera_imagen");
-    imagenes[2] = document.getElementById("segunda_imagen");
-    imagenes[3] = document.getElementById("tercera_imagen");
-    imagenes[4] = document.getElementById("cuarta_imagen");
-    preguntas[1] = document.getElementById("pregunta_uno");
-    preguntas[2] = document.getElementById("pregunta_dos");
-    preguntas[3] = document.getElementById("pregunta_tres");
-    preguntas[4] = document.getElementById("pregunta_cuatro");
-
+    s = document.getElementById("segundos");
+    m = document.getElementById("minutos");
 }
     
 function cambiarSplash(){
@@ -39,36 +30,35 @@ function cambiarSeccion(id_seccion){
 
 }
 
-function puntuacion(id_pregunta){
-    for (var i in preguntas) {
-        secciones[i].classList.add("oculto");
-    }
-    secciones[id_seccion].classList.remove("oculto");
-
-}
-
-function carga(){
+function comenzarJuego(){
+    secciones[2].className = "inicio oculto";
+    secciones[3].className = "juego";
     contador_s=0;
     contador_m=0;
-    s = document.getElementById("segundos");
-    m = document.getElementById("minutos");
     
-    window.setInterval(
-        function(){
-            if(contador_s==60){
-                
-                contador_s=0;
-                contador_m++;
-                m.innerHTML = contador_m;
-                
-                if(contador_m==0){
-                    
-                    contador_m=0;
-                }
-            }
+    activarTiempo();
+}
+
+function activarTiempo(){
+    if(id_intervalo){
+        clearInterval(id_intervalo);
+    }
+    id_intervalo = setInterval(tiempo,1000);
+}
+
+function tiempo(){
+    if(contador_s==60){
+        
+        contador_s=0;
+        contador_m++;
+        m.innerHTML = contador_m;
+        
+        if(contador_m==0){
             
-            s.innerHTML = contador_s;
-            contador_s++;
+            contador_m=0;
         }
-        ,1000);
+    }
+    
+    s.innerHTML = contador_s;
+    contador_s++;
 }
