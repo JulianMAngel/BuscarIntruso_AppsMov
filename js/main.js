93e7b76127c696ad8be1c953c64dcf00bcceb298
FelipeFranco97 <francozuluaga97@gmail.com>
2020-03-04 20:47:00 -0500

    Update Css y Js
    Se modifico principalmente el javascript, arreglando un par de bugs que se tenían:
    
    - El primer nivel ahora se ve correctamente después de haber jugado previamente.
    
    - Los minutos acumulados de una partida, no quedan guardados para una partida siguiente.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var secciones = [];
 var preguntas = [];
 var tiempo_splash = 3000;
+var stop;
 var id_intervalo;
 var score = 1;
 
@@ -8,17 +9,18 @@ window.onload = function(){
     inicializarReferencias();
     setTimeout(cambiarSplash,tiempo_splash);
 }
-    
+
 function inicializarReferencias(){
     secciones[1] = document.getElementById("seccion_1");
     secciones[2] = document.getElementById("seccion_2");
     secciones[3] = document.getElementById("seccion_3");
     secciones[4] = document.getElementById("seccion_4");
     secciones[5] = document.getElementById("seccion_5");
-    preguntas[1] = document.getElementById("primera_pregunta")
-    preguntas[2] = document.getElementById("segunda_pregunta")
-    preguntas[3] = document.getElementById("tercera_pregunta")
-    preguntas[4] = document.getElementById("cuarta_pregunta")
+    preguntas[1] = document.getElementById("primera_pregunta");
+    preguntas[2] = document.getElementById("segunda_pregunta");
+    preguntas[3] = document.getElementById("tercera_pregunta");
+    preguntas[4] = document.getElementById("cuarta_pregunta");
+    stop = document.getElementById("menu_stop");
     s = document.getElementById("segundos");
     m = document.getElementById("minutos");
 }
@@ -33,7 +35,6 @@ function cambiarSeccion(id_seccion){
         secciones[i].classList.add("oculto");
     }
     secciones[id_seccion].classList.remove("oculto");
-
 }
 
 function comenzarJuego(){
@@ -41,7 +42,11 @@ function comenzarJuego(){
     secciones[4].className = "instrucciones oculto";
     secciones[5].className = "creditos oculto";
     secciones[3].className = "juego";
-    preguntas[1].className = "pregunta_uno";
+    preguntas[1].className = "pregunta1";
+    preguntas[2].className = "pregunta2 oculto";
+    preguntas[3].className = "pregunta3 oculto";
+    preguntas[4].className = "pregunta4 oculto";
+    document.getElementById("minutos").innerHTML = "0";
     contador_s=0;
     contador_m=0;
     
@@ -70,6 +75,10 @@ function tiempo(){
     
     s.innerHTML = contador_s;
     contador_s++;
+}
+
+function parar(){
+    stop.className = "pausa";
 }
 
 function respuestaIncorrecta(id_seccion){
